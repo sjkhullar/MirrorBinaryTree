9f8482fa6e061a16b6effa18cb6e71e10b3956bf
geeks2champ <geeks2champ@gmail.com>
2016-02-21 15:20:52 +0530

    Update mirror binary tree code

--- MirrorBinaryTree/src/main/java/org/learn/Client/App.java
+++ MirrorBinaryTree/src/main/java/org/learn/Client/App.java
@@ -1,27 +1,34 @@
 package org.learn.Client;
 
-import org.learn.PrepareTree.Node;
-import org.learn.PrepareTree.Tree;
 import org.learn.Question.MirrorTree;
+import org.learn.Question.Node;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-	public static void main( String[] args )
-    {		
-		int[] data = {50, 150, 25, 80, 125, 160};
-    	Tree tree = new Tree();
-    	Node root = tree.createNode(100);
-    	for(int i : data ) {
-    		tree.insert(root, i );
-    	}   
-    	System.out.println("Binary Tree");
-    	tree.printTree(root);    	
-    	MirrorTree.mirrorTree(root);
-    	System.out.println("Mirror Binary Tree");
-    	tree.printTree(root);
-    }
+public class App {
+	public static void main(String[] args) {
+		// root level 0
+		Node A = Node.createNode(55);
+		// Level 1
+		Node B = Node.createNode(50);
+		Node C = Node.createNode(40);
+		// Level 2
+		Node D = Node.createNode(25);
+		Node E = Node.createNode(80);
+		Node F = Node.createNode(45);
+		Node G = Node.createNode(90);
+
+		// connect Level 0 and 1
+		A.left = B;
+		A.right = C;
+		// connect level 1 and level 2
+		B.left = D;
+		B.right = E;
+		C.left = F;
+		C.right = G;
+		
+		System.out.println("Binary Tree : ");
+		MirrorTree.printTree(A);
+		MirrorTree.mirrorTree(A);
+		System.out.println("Mirror Binary Tree : ");
+		MirrorTree.printTree(A);
+	}
 }
--- MirrorBinaryTree/src/main/java/org/learn/Question/MirrorTree.java
+++ MirrorBinaryTree/src/main/java/org/learn/Question/MirrorTree.java
@@ -1,7 +1,8 @@
 
 package org.learn.Question;
 
-import org.learn.PrepareTree.Node;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MirrorTree {
 	public static void mirrorTree(Node root) {
@@ -13,6 +14,27 @@ public class MirrorTree {
 		Node swapNode = root.left;
 		root.left = root.right;
 		root.right = swapNode;
+		return;
+	}
+	
+	public static void printTree(Node root) {
+		if (root == null) {
+			System.out.println("Tree is empty");
+			return ;
+		}
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			Node node = queue.poll();			
+			System.out.printf("%d ",node.data);		
+			if (node.left != null) {
+				queue.offer(node.left);
+			}
+			if (node.right != null) {
+				queue.offer(node.right);
+			}
+		}
+		System.out.println("");	
 		return;
 	}
 }
